552e607f631ce4487684e3cf52ac5a6fead817f9
bdurb <brian@rpmgreenville.com>
2018-02-13 13:24:33 -0500

    set up get, post, search, and delete

--- node-express-walkthrough/server.js
+++ node-express-walkthrough/server.js
@@ -1,38 +1,76 @@
+import { read } from 'fs';
+
 const express = require('express');
 const bodyParser = require('body-parser';)
 
 const app = express();
+const PORT = 3030;
+const USER_ERROR = 422;
 
-const PORT = 3030
+let users = [];
+let id = 0;
+
 app.use(bodyParser.json());
 
-const name = [];
-const animals = [];
-let userId = 0;
-
-app.use('/', (req, res, next) => {
-  console.log('Request type:', req.method);
-  res.send('<h1>You are the winner</h1>');
-  next();
+app.post('/users', (req, res) => {
+  const name = req.body.user;
+  if (!name) {
+    res.status(USER_ERROR);
+    res.send("Must provide a user name");
+  } else if (typeof user !== 'string') {
+    res.status(USER_ERROR);
+    res.send("name must be a string");
+  } else {
+    id++
+    users.push([id, name]);
+    res.status(200);
+    res.send(id + "");
+  }
 });
 
-app.get('/names/:name', (req,res) => {
-  res.send(names[req.params.name] || "User does not exist");
+app.get('/users', (req, res) => {
+  const userNames = users.map(user => user[1]);
+  res.status(200);
+  res.json(userNames)
 });
 
-app.post('/names', (req, res) => {
-  names[req.body.name] = userId;
-  userId++;
-  res.send(userId);
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (typeof id !== 'number') {
+    res.status(USER_ERROR);
+    res.send("An id is required as a number");
+    return;
+  }
+  const requestedUser = users.filter(user => {
+    return user[0] === id;
+  })[0];
+  if (requestedUser.length < 1) {
+    res.status(USER_ERROR);
+    res.send("matching id must be provided");
+    return;
+  }
+  res.status(200);
+  res.send(requestedUser[1]);
 });
 
-// app.get('/', (req,res) => {
-//   console.log('this was a get request');
-// });
+app.get('/search', (req, res) => {
+  const  name = req.query.name;
+  const matchingUsers = users.filter(user => {
+    return user[1] === name;
+  });
+  res.status(SUCCESS);
+  res.json(matchingUsers);
+});
 
-// app.post('/', (req, res) => {
-//   console.log('This was a post request')
-// });
+app.delete('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  users = users.filter(user => {
+    return user[0] !== id;
+  });
+  res.status(SUCCESS);
+  res.json(users);
+})
+
 
 app.listen(PORT, err => {
   if (err) {
